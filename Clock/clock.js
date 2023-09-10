580635c79606c2f290fa90cc7c55279a9007316f
Mert Sancar <msmertsancar@gmail.com>
2023-09-10 21:14:01 +0300

    first commit

---
+++ Clock/clock.js
@@ -0,0 +1,31 @@
+const myClockElement = document.getElementById('myClock');
+const myNameElement = document.getElementById('myClock');
+
+
+function showTime() {
+    const currentDate = new Date();
+    const timeString = currentDate.toUTCString();
+
+    myClockElement.textContent = timeString;
+}
+
+let username = null;
+
+function askUserName() {
+    username = prompt("Adınız nedir?");
+    if (username) {
+        myNameElement.textContent = username;
+    }
+}  
+askUserName();   /*
+let userName = localStorage.getItem('username');
+if (!userName) {
+    userName = prompt("Adınız nedir?");
+if (userName) {
+    localStorage.setItem('username', userName);
+    }
+}
+myNameElement.textContent = userName;  */
+showTime();
+
+setInterval(showTime, 1000);
