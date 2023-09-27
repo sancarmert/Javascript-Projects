6b3e0154c0756bb4f211f43d9277c044e42aebea
Mert Sancar <msmertsancar@gmail.com>
2023-09-27 18:54:47 +0300

    first commit

---
+++ Calculator/script.js
@@ -0,0 +1,66 @@
+let currentInput = '';
+let result = '';
+
+function appendToDisplay(value) {
+    currentInput += value;
+    document.getElementById('display').value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    result = '';
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        result = eval(currentInput);
+        document.getElementById('display').value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        document.getElementById('display').value = 'Hata';
+        currentInput = '';
+    }
+}
+
+function power() {
+    try {
+        currentInput += '**';
+        document.getElementById('display').value = currentInput;
+    } catch (error) {
+        document.getElementById('display').value = 'Hata';
+        currentInput = '';
+    }
+}
+
+function squareRoot() {
+    try {
+        currentInput = Math.sqrt(eval(currentInput));
+        document.getElementById('display').value = currentInput;
+    } catch (error) {
+        document.getElementById('display').value = 'Hata';
+        currentInput = '';
+    }
+}
+
+function modulo() {
+    try {
+        currentInput += '%';
+        document.getElementById('display').value = currentInput;
+    } catch (error) {
+        document.getElementById('display').value = 'Hata';
+        currentInput = '';
+    }
+}
+
+// Klavyeden tuşlara tepki verme
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    if (/[0-9+\-*/.^%]/.test(key)) {
+        appendToDisplay(key);
+    } else if (key === 'Enter') {
+        calculateResult();
+    } else if (key === 'Escape') {
+        clearDisplay();
+    }
+});
