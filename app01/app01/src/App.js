10cdb16b3306600aaa0d79c268ec6dfecefe5fce
Mert Sancar <msmertsancar@gmail.com>
2023-10-07 17:42:04 +0300

    modify

--- app01/app01/src/App.js
+++ app01/app01/src/App.js
@@ -1,23 +1,23 @@
-import React, { useEffect} from 'react';
+import React, { useEffect } from 'react';
 import getData from './getData';
 
-function App(){
-
+function App() {
     useEffect(() => {
+        // getData(1) işlemi burada gerçekleşecek
         getData(1)
-        .then((userData) => {
-            console.log('Kullanıcı Verileri', userData);
-        })
-        .catch((error) => {
-            console.error('Hata',error);
-        });  
-}, [] );
+            .then((userData) => {
+                console.log('Kullanıcı Verileri', userData);
+            })
+            .catch((error) => {
+                console.error('Hata', error);
+            });
+    }, []);
 
-return(
-    <div className="App">
-        {/* Uygulama içeriği */}
-    </div>
-);
+    return (
+        <div className="App">
+            {/* Uygulama içeriği */}
+        </div>
+    );
 }
 
-export default App;
+export default App;
